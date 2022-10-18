0fa80ee3c7f88c5f22ac296392e57d2e5739d12f
almyrick <myrick.ashley1015@yahoo.com>
2022-10-18 14:28:43 -0400

    parent stub

--- Parent.java
+++ Parent.java
@@ -1,5 +1,15 @@
-package CSCE247Project;
-
+import java.util.ArrayList;
 public class Parent {
-    
+   public String firstName;
+   public String lastName;
+   private String email;
+   private int number;
+   private ArrayList<Child> childList;
+   
+public void addChild(String childFirstName, String childLastName, int childAge, Restriction restrictions){
+
+}
+public String toString(){
+
+}
 }
