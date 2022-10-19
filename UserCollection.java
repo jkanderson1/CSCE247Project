78bc9186c33650e41bf4842dd6d2c6ca1e2fc728
almyrick <myrick.ashley1015@yahoo.com>
2022-10-18 23:40:52 -0400

    stubs

--- UserCollection.java
+++ UserCollection.java
@@ -3,5 +3,24 @@ public class UserCollection {
     public String password;
     public Schedule schedule;
 
-    
+    private UserCollection(){
+
+    }
+    public UserCollection getinstance(){
+        return null;
+        
+    }
+    public void addUser(String firstname, String lastname){
+
+    }
+    public User getUser(String UserName){
+        return null;
+        
+    }
+    public void editUser(){
+
+    }
+    public void saveUsers(){
+        
+    }
 }
