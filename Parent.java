557de67c5a0b698f0c7664bf28443f2db4e1c92b
almyrick <myrick.ashley1015@yahoo.com>
2022-10-20 14:15:00 -0400

    changes

--- Parent.java
+++ Parent.java
@@ -2,18 +2,14 @@ import java.util.ArrayList;
 public interface Parent {
    public String firstName;
    public String lastName;
-   String email;
-   int number;
-   ArrayList<Child> childList;
+   public String email;
+   public int number;
    
 /*public void addChild(String childFirstName, String childLastName, int childAge, Restriction restrictions){
 
 } */
-public Parent (String firstName, string lastName, String email, int number){
-   this.firstName= firstName;
+public static void addParent(String firstName, String lastName, String email, int number){
+// call data writer to add parent to the jason parent 
 }
 
-public String toString(){
-   return "";
-}
 }
