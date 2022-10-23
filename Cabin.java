12f27b37a9143362b134e5c5e25f79c7bd434efb
Colson Carey <damegapic@gmail.com>
2022-10-23 19:11:05 -0400

    cabin updates

--- Cabin.java
+++ Cabin.java
@@ -1,15 +1,22 @@
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cabin {
     private Counselor counselor;
-    public ArrayList<Child> chidlren;
-    public String CabinName;
-    public String cabinNumber;
+    public ArrayList<Child> children;
+    public String cabinName;
+    public int cabinNumber;
     public int Sessions;
+    private Scanner keyboard = new Scanner(System.in);
 
-    public Cabin makeCabin(String etageGroup){
-        return null;    
+    public Cabin makeCabin(String ageGroup, Counselor counselor){
+        // this.children = ChildCollection.childList(ageGroup);
+        this.counselor = counselor;
+        System.out.println("Input New Cabin Name");
+        this.cabinName = keyboard.nextLine();
+        System.out.println("Input New Cabin Number");
+        this.cabinNumber = keyboard.nextInt();
     }
 
     public String setageGroup(ArrayList<Child> children){
