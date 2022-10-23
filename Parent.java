18bf4bd96b52fa1573e9ab03a028ffb667172e09
Jordyn Anderson <jordynanderson@Jordyns-MacBook-Pro-4.local>
2022-10-23 18:54:18 -0400

    changes in counselor and counselor collection

--- Parent.java
+++ Parent.java
@@ -3,12 +3,13 @@ public class Parent extends User{
    public String firstName;
    public String lastName;
    public String email;
-   public int number;
+   public String number;
+   public String address;
    
 /*public void addChild(String childFirstName, String childLastName, int childAge, Restriction restrictions){
 
 } */
-public static void addParent(String firstName, String lastName, String email, int number){
+public static void addParent(String firstName, String lastName, String email, String number, String address){
 // call data writer to add parent to the jason parent 
 }
 
