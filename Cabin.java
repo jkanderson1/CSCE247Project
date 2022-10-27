bd99b7a2a430a009b51151b2511a47ccf6221331
Colson Carey <damegapic@gmail.com>
2022-10-27 14:24:52 -0400

    json updates

--- Cabin.java
+++ Cabin.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.util.UUID;
 
 public class Cabin {
-    public static Cabin cabin;
     public Counselor counselor;
     public ArrayList<Child> children;
     public int cabinNumber;
@@ -23,42 +22,10 @@ public class Cabin {
         this.counselorUUID = counselorUUID;
         this.session = session;
     }
+    public void fillCabin(){
 
-    public static Cabin makeCabinYoungest(int maxAge,int minAge, String counselorUUID, Counselor counselor, int session){
-        //Singleton
-        maxAge = 5;
-        minAge = 9;
-        if(cabin == null)
-        {
-            System.out.println("Creating a cabin for the youngest age group");
-            cabin = new Cabin(maxAge, minAge, counselorUUID, counselor, session);
-        }
-        return cabin;
     }
 
-    public static Cabin makeCabinMiddleCabin(int maxAge,int minAge, String counselorUUID, Counselor counselor, int session){
-        //Singleton
-        maxAge = 9;
-        minAge = 14;
-        if(cabin == null)
-        {
-            System.out.println("Creating a cabin for the middle age group");
-            cabin = new Cabin(maxAge, minAge, counselorUUID, counselor, session);
-        }
-        return cabin;
-    }
-
-    public static Cabin makeCabinOldest(int maxAge,int minAge, String counselorUUID, Counselor counselor, int session){
-        //Singleton
-        maxAge = 14;
-        minAge = 18;
-        if(cabin == null)
-        {
-            System.out.println("Creating a for the oldest age group");
-            cabin = new Cabin(maxAge, minAge, counselorUUID, counselor, session);
-        }
-        return cabin;
-    }
     //make maximum amount of children per cabin and keep track of each child
 
 
