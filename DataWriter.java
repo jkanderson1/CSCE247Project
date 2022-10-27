e501717983e34fafe1547c4cb81fb59ec49fd88a
WalkerBowen <walkerbowen2002@gmail.com>
2022-10-26 21:16:13 -0400

    dataWriter

--- DataWriter.java
+++ DataWriter.java
@@ -1,5 +1,6 @@
 //Written by Walker Bowen
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
@@ -9,50 +10,58 @@ import org.json.simple.*;
 public class DataWriter {
 
 public boolean saveAllChildren(ArrayList<Child> children)
-{FileWriter childFile = new FileWriter("Child.json");
-    java.util.Iterator<Child> iterator = children.iterator();
-   while(iterator.hasNext())
-   {
-    Child child = iterator.next();
-    JSONObject jsonChild = new JSONObject();
-    jsonChild.put("childFirstname",child.childFirstname);
-    jsonChild.put("childLastName", child.childLastname);
-    jsonChild.put("childDOB", child.childDOB);
-    jsonChild.put("restriction", child.restriction);
-    //need to create new pediatricianArray for contacts
-    JSONArray pediatricianArray = new JSONArray();
-    pediatricianArray.add("firstname:"+" "+child.Pediatrician.firstname);
-    pediatricianArray.add("lastname:"+" "+child.Pediatrician.lastname);
-    pediatricianArray.add("number:"+" "+child.Pediatrician.number);
-    pediatricianArray.add("address:"+" "+child.Pediatrician.address);
-
-    jsonChild.put("pediatrician", pediatricianArray);
-
-    JSONArray EContactArray = new JSONArray();
-
-    EContactArray.add("firstname:"+" "+child.EmergencyContact.firstname);
-    EContactArray.add("lastname:"+" "+child.EmergencyContact.lastname);
-    EContactArray.add("number:"+" "+child.EmergencyContact.number);
-    EContactArray.add("address:"+" "+child.EmergencyContact.address);
-    
-    jsonChild.put("emergencyContact", EContactArray);
-
-    childFile.write(jsonChild.toJSONString());
+{
+    JSONArray jsonChildren = new JSONArray();
+    for (int i = 0; i<children.size(); i++) 
+    {
+        
+        jsonChildren.add(getChildJSON(children.get(i)));
 
     }
-    childFile.close();
-    return true; 
+    try(FileWriter childFile = new FileWriter("Child.json"))
+    {
+        childFile.write(jsonChildren.toJSONString());
+        childFile.flush();
+        return true;
+        
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+        return false;
+    }
 
 
-   }
+   
 
     
+}
+public static JSONObject getChildJSON(Child child)
+{
+    JSONObject childDetails = new JSONObject();
+    childDetails.put("childFirstName",child.childFirstname);
+    childDetails.put("childLastName", child.childLastname);
+    childDetails.put("childAge", child.childDOB);
+    childDetails.put("restrictions", child.restriction);
+    childDetails.put("emergencyContact", child.EmergencyContact.firstname+" "+child.EmergencyContact.lastname);
+    childDetails.put("emergencyContactNumber", child.EmergencyContact.number);
+    childDetails.put("pediatrician", child.Pediatrician.firstname+" "+child.Pediatrician.lastname);
+    childDetails.put("pediatricianNumber", child.Pediatrician.number);
+    childDetails.put("UUID", child.childID.toString());
+    
+    return childDetails;
+
 }
 
 public boolean saveAllCabins(ArrayList<Cabin> cabins)
 {
     
     
+}
+
+public static JSONObject getCabinJson()
+{
+    JSONObject cabinDetails = new JSONObject();
+
 }
 
 public boolean saveAllContacts(ArrayList<Contact> contacts)
