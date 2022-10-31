16232fbd0f9cb8ca6892b693325b3a7109f91f25
Jordyn Anderson <jordynanderson@Jordyns-MacBook-Pro-4.local>
2022-10-31 16:51:18 -0400

    activities methods

--- Schedule.java
+++ Schedule.java
@@ -2,10 +2,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Schedule{
-    public ArrayList<ChildActivities> ChildActivities = new ArrayList<ChildActivities>();
-    public ArrayList<TeenActivities> TeenActivities = new ArrayList<TeenActivities>();
-    private HashMap <Days,ArrayList<ChildActivities>>ChildHMap = new HashMap <Days,ArrayList<ChildActivities>>();
-    private HashMap <Days,ArrayList<TeenActivities>>TeenHMap = new HashMap <Days,ArrayList<TeenActivities>>();
+    public ArrayList<Activities> Activities = new ArrayList<Activities>();
+    private HashMap <Days,ArrayList<Activities>>ActivitiesHMap = new HashMap <Days,ArrayList<Activities>>();
+   
+    public void makeScheduleOne(){
+        ActivitiesHMap.put(Days.Monday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Tuesday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Wednesday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Thursday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Friday, new ArrayList<Activities>());
+    }
+
+    public void makeScheduleTwo(){
+        ActivitiesHMap.put(Days.Monday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Tuesday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Wednesday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Thursday, new ArrayList<Activities>());
+        ActivitiesHMap.put(Days.Friday, new ArrayList<Activities>());
+    }
+
 }
 
 
