448eb29138ca6920921fcaf4d8ff441105e2b4c3
Jordyn Anderson <jordynanderson@Jordyns-MacBook-Pro-4.local>
2022-11-02 18:05:46 -0400

    adding java doc

--- Director.java
+++ Director.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * Director class that stores all the properties of the director
+ */
 public class Director extends User{
 private ArrayList<Child> children;
 private ArrayList<String> parentname;
@@ -15,6 +18,15 @@ public ChildCollection child;
 public CounselorCollection counselor;
 public CabinCollection cabins;
 
+/**
+ * Creating director that has the following parameters
+ * @param username director username
+ * @param password director password
+ * @param firstName director first name
+ * @param lastName director last name
+ * @param emergencyContact director emergency contact
+ * @param restrictions director restrictions
+ */
 public Director(String username, String password, String firstName, String lastName, Contact emergencyContact, String restrictions)
 {
     super(username, password);
@@ -24,22 +36,36 @@ public Director(String username, String password, String firstName, String lastN
     this.restrictions = restrictions; 
     
 }
-    /*
+    /**
      * A static method that generates a UUID randomly
+     * @return the UUID
      */
     private static UUID setUUID(){
         UUID temp = UUID.randomUUID();
         return temp;
     }
-
+/**
+ * View cabins method that returns the whole array list of cabins
+ * @param cabinList
+ * @return list of cabins from cabin collection
+ */
 private CabinCollection viewCabins(ArrayList<Cabin> cabinList){
     return (CabinCollection)cabins;
 
 }
+/**
+ * view child method that returns the whole array list of children
+ * @param Children
+ * @return list of children from child collection
+ */
 private ChildCollection viewChildren(ArrayList<Child> Children){
     return (ChildCollection)child;
-
 }
+/**
+ * 
+ * @param counselors
+ * @return
+ */
 private CounselorCollection viewCounselors(ArrayList<Counselor>counselors){
     return (CounselorCollection)counselor;
 
