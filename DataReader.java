7dcbd8a89969f1f8532b3bf3da47c045264f3643
Colson Carey <damegapic@gmail.com>
2022-11-02 21:03:10 -0400

    minor fix

--- DataReader.java
+++ DataReader.java
@@ -224,7 +224,7 @@ public class DataReader {
                     String pAddress = (String)pediatricianJson.get("Address");
                     Contact pediatrician = new Contact(pFirstName, pLastName, pNumber, pAddress);
                     Child child = new Child(childFirstName, childLastName, childAge, childRestrictions, Econtact, pediatrician);
-                    System.out.println(child.toString());
+                    childrenAL.add(child);
 
                 }
                 parentAL.add(new Parent(username, password, firstName, lastName, email, number, address, childrenAL));
