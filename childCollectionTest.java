7ceafc81d87ae310ad4e2138b79fec53763cd8d0
almyrick <myrick.ashley1015@yahoo.com>
2022-11-11 17:22:08 -0500

    testing

--- childCollectionTest.java
+++ childCollectionTest.java
@@ -5,3 +5,50 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+class childCollectionTest{
+    private ChildCollection childcollection = ChildCollection.getChild();
+    private static ArrayList<Child> children = ChildCollection.getChild(0, 0);
+
+    @BeforeEach
+    public void set(){
+        children.clear();
+        children.add(new Child("Sally", "Martin","10/12/2010","none",(new Contact("Bob", "Manning", "8031234567", "123 house drive")),(new Contact("Jonny", "Cash", "8031234567", "123 Barbie drive"))));
+        children.add(new Child("Mike", "Sully","02/22/2010","none",(new Contact("Blake", "Uni", "8031234567", "123 lake drive")),(new Contact("Jonny", "Cash", "8031234567", "123 Barbie drive"))));
+    }
+
+    @AfterEach
+    public void tearDown(){
+        ChildCollection.getChild(0, 0)
+    }
+
+    @Test
+    void testHaveUserValidFirstname() {
+		boolean hasSally = childcollection.haveChild("Sally");
+		assertTrue(hasSally);
+	}
+    @Test
+	void testHaveUserValidLastname() {
+		boolean hasMike = childcollection.haveChild("bwhite");
+		assertTrue(hasMike);
+	}
+
+    @Test
+	void testHaveUserInValid() {
+		boolean hasSophie = childcollection.haveChild("jsmith");
+		assertFalse(hasSophie);
+	}
+
+    @Test
+	void testHaveUserEmpty() {
+		boolean hasEmpty = childcollection.haveChild("");
+		assertFalse(hasEmpty);
+	}
+
+    @Test
+	void testHaveUserNull() {
+		boolean hasNull = childcollection.haveChild(null);
+		assertFalse(hasNull);
+	}
+
+}
