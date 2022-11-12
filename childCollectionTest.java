bd1399bca7530b040bd12e5e3dc9bd0187d8c88b
almyrick <myrick.ashley1015@yahoo.com>
2022-11-11 19:04:00 -0500

    testing

--- childCollectionTest.java
+++ childCollectionTest.java
@@ -7,19 +7,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class childCollectionTest{
-    private ChildCollection childcollection = ChildCollection.getChild();
-    private static ArrayList<Child> children = ChildCollection.getChild(0, 0);
+    private ChildCollection childcollection = ChildCollection.getinstance();
+    private static ArrayList<Child> children = ChildCollection.getAllChildren();
 
     @BeforeEach
     public void set(){
         children.clear();
         children.add(new Child("Sally", "Martin","10/12/2010","none",(new Contact("Bob", "Manning", "8031234567", "123 house drive")),(new Contact("Jonny", "Cash", "8031234567", "123 Barbie drive"))));
         children.add(new Child("Mike", "Sully","02/22/2010","none",(new Contact("Blake", "Uni", "8031234567", "123 lake drive")),(new Contact("Jonny", "Cash", "8031234567", "123 Barbie drive"))));
+        
     }
 
     @AfterEach
     public void tearDown(){
-        ChildCollection.getChild(0, 0)
+        ChildCollection.getinstance().getkids().clear();
     }
 
     @Test
